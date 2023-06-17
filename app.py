88b0920baef5ddcad8e72d824823df86ee4be8f1
GuilhermeVolpato <volpato2103@gmail.com>
2023-06-17 16:00:47 -0300

    feat: CRUD de cardápio finalizado

--- app.py
+++ app.py
@@ -26,12 +26,32 @@ def home():
 @app.post('/cardapio/insert')
 def create_cardapio():
     data = request.json
-    id_cardapio = data['id_cardapio']
-    nome = data['nome']
+    id_item_cardapio = data['id_item_cardapio']
+    nome_item = data['nome_item']
+    valor = data['valor']
+    descricao = data['descricao']
+    categoria = data['categoria']
+    disponibilidade = data['disponibilidade']
+    id_estoque = data['id_estoque']
+    quantidade = data['quantidade']
+    unidade = data.get('unidade')
     cursor = conn.cursor()
-    cursor.execute(f"INSERT INTO cardapio (id_cardapio, nome) VALUES (?, ?)", (id_cardapio, nome))
-    conn.commit()
-    return {'message': 'Funcionário cadastrado com sucesso!'}
+    cursor.execute("SELECT COUNT(*) FROM estoque WHERE id_estoque = ?", (id_estoque,))
+    result = cursor.fetchone()
+
+    if result[0] > 0:  # Verifica se o valor de id_estoque existe na tabela estoque
+        cursor.execute("INSERT INTO cardapio (id_item_cardapio, nome_item, valor, descricao, categoria, disponibilidade) VALUES (?, ?, ?, ?, ?, ?)",
+                       (id_item_cardapio, nome_item, valor, descricao, categoria, disponibilidade))
+        if unidade is not None:
+            cursor.execute("INSERT INTO ingredientes (id_item_cardapio, id_estoque, quantidade, unidade) VALUES (?, ?, ?, ?)",
+                           (id_item_cardapio, id_estoque, quantidade, unidade))
+        else:
+            cursor.execute("INSERT INTO ingredientes (id_item_cardapio, id_estoque, quantidade) VALUES (?, ?, ?)",
+                           (id_item_cardapio, id_estoque, quantidade))
+        conn.commit()
+        return {'message': 'Item cadastrado com sucesso!'}
+    else:
+        return {'error': 'Não foi possível cadastrar o item, pois o id_estoque informado não existe na tabela estoque.'}
 
 
 @app.get('/cardapio/read')
@@ -40,31 +60,72 @@ def read_cardapio():
     cursor.execute('SELECT * FROM cardapio')
     rows = cursor.fetchall()
 
-    funcionarios = []
+    cardapios = []  # formata a resposta da requisição
     for row in rows:
-        funcionario = {
-            'id_cardapio': row.id_cardapio,
-            'nome': row.nome
+        cardapio = {
+            'id_item_cardapio': row.id_item_cardapio,
+            'nome_item': row.nome_item,
+            'valor': row.valor,
+            'descricao': row.descricao,
+            'categoria': row.categoria,
+            'disponibilidade': row.disponibilidade
         }
-        funcionarios.append(funcionario)
+        cardapios.append(cardapio)
 
-    return {'cardapio': funcionarios}
+    return {'cardapio': cardapios}
 
 @app.post('/cardapio/update')
 def update_cardapio():
     data = request.json
-    id_cardapio = data['id_cardapio']
-    nome = data['nome']
+    id_item_cardapio = data['id_item_cardapio']
+    nome_item = data.get('nome_item')
+    valor = data.get('valor')
+    descricao = data.get('descricao')
+    categoria = data.get('categoria')
+    disponibilidade = data.get('disponibilidade')
+    
     cursor = conn.cursor()
-    cursor.execute(f"UPDATE cardapio SET nome = ? WHERE id_cardapio = ?", (nome, id_cardapio))
+    update_values = []
+    query = "UPDATE cardapio SET "
+
+    if nome_item:
+        query += "nome_item = ?, "
+        update_values.append(nome_item)
+    
+    if valor:
+        query += "valor = ?, "
+        update_values.append(valor)
+
+    if descricao:
+        query += "descricao = ?, "
+        update_values.append(descricao)
+
+    if categoria:
+        query += "categoria = ?, "
+        update_values.append(categoria)
+
+    if disponibilidade is not None:
+        query += "disponibilidade = ?, "
+        update_values.append(disponibilidade)
+
+    # Remove a última vírgula e espaço da query
+    query = query[:-2]
+    
+    query += " WHERE id_item_cardapio = ?"
+    update_values.append(id_item_cardapio)
+
+    cursor.execute(query, update_values)
     conn.commit()
-    return {'message': 'cardapio atualizado com sucesso!'}
+    
+    return {'message': 'Cardápio atualizado com sucesso!'}
+
 
 @app.post('/cardapio/delete')
 def delete_cardapio():
     data = request.json
-    id_cardapio = data['id_cardapio']
+    id_item_cardapio = data['id_item_cardapio']
     cursor = conn.cursor()
-    cursor.execute(f"DELETE FROM cardapio WHERE id_cardapio = ?", (id_cardapio))
+    cursor.execute(f"DELETE FROM ingredientes WHERE id_item_cardapio = ?", (id_item_cardapio,))
+    cursor.execute(f"DELETE FROM cardapio WHERE id_item_cardapio = ?", (id_item_cardapio))
     conn.commit()
     return {'message': 'cardapio deletado com sucesso!'}
